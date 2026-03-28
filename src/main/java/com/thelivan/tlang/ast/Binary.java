e17300f9939e60571a531434ced3aab01448754c
Mark Zharkov <markjarkovkod@gmail.com>
2026-03-28 11:49:02 +0300

    refactor: add @Getter annotation to Binary

--- src/main/java/com/thelivan/tlang/ast/Binary.java
+++ src/main/java/com/thelivan/tlang/ast/Binary.java
@@ -1,10 +1,14 @@
 package com.thelivan.tlang.ast;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class Binary extends Expression {
+  @Getter
   private final Expression left;
+  @Getter
   private final Expression right;
+  @Getter
   private final Operation operation;
 }
