12a395c5b24972f4fd27daae52363820d0c07d2d
Mark Zharkov <markjarkovkod@gmail.com>
2026-03-28 11:50:40 +0300

    refactor: add @RequiredArgsConstructor annotation to Unary

--- src/main/java/com/thelivan/tlang/ast/Unary.java
+++ src/main/java/com/thelivan/tlang/ast/Unary.java
@@ -1,15 +1,12 @@
 package com.thelivan.tlang.ast;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class Unary extends Expression {
   @Getter
   private final Operation operation;
   @Getter
   private final Expression expression;
-
-  public Unary(Operation operation, Expression expression) {
-    this.operation = operation;
-    this.expression = expression;
-  }
 }
