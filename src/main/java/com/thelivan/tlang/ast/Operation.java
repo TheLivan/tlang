e9dc2f3d4bf7d9048b4109518632cbcf9eea82bb
Mark Zharkov <markjarkovkod@gmail.com>
2026-04-03 13:33:20 +0300

    feat: add Operation.fromToken and fromTokenUnary

--- src/main/java/com/thelivan/tlang/ast/Operation.java
+++ src/main/java/com/thelivan/tlang/ast/Operation.java
@@ -12,6 +12,16 @@ public enum Operation {
 
   public static Operation fromToken(TokenKind kind) {
     return switch (kind) {
+      case PLUS -> ADD;
+      case MINUS -> SUB;
+      case ASTERISK -> MUL;
+      case SLASH -> DIV;
+      default -> throw new IllegalArgumentException("not binary");
+    };
+  }
+
+  public static Operation fromTokenUnary(TokenKind kind) {
+    return switch (kind) {
       case PLUS -> U_POS;
       case MINUS -> U_NEG;
       default -> throw new IllegalArgumentException("not unary");
